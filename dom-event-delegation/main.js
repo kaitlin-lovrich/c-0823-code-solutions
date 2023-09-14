d6deeab43391ef049741d71f2bcaf1d4c5cd7a95
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-09-14 20:45:54 +0000

    Updated js file

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -3,6 +3,11 @@ const $taskList = document.querySelector('.task-list');
 function target(event) {
   console.log('Target:', event.target);
   console.log('Target tagName:', event.target.tagName);
+
+  if (event.target.tagName !== 'BUTTON') {
+    return;
+  }
+
   const closest = event.target.closest('.task-list-item');
   console.log('Closest:', closest);
   closest.remove();
