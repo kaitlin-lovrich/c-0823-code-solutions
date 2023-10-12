720e2c97bf017b6da8f0e454277bbb2d67314920
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-10-12 21:24:57 +0000

    Completed react-props-and-expressions

---
+++ react-props-and-expressions/my-app/src/App.jsx
@@ -0,0 +1,28 @@
+// import { useState } from 'react'
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import CustomButton from './CustomButton';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <CustomButton text={'I'} color={'pink'} />
+      <CustomButton text={'know'} color={'purple'} />
+      <CustomButton text={'React'} color={'blue'} />
+    </>
+  );
+}
+
+export default App;
