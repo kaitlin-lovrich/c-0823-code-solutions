fb0cbcbde56a57c02e78d45a51a9e70967cc3a1e
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-10-18 22:03:30 +0000

    Completed node-fs-writefile exercise -Kaitlin Lovrich

--- node-fs-writefile/note-to-self.js
+++ node-fs-writefile/note-to-self.js
@@ -2,7 +2,6 @@ import { writeFile } from 'node:fs/promises';
 
 async function noteToSelf() {
   const fileContents = process.argv[2] + '\n';
-  if (fileContents) throw new Error('Null Input. Add note.');
   // eslint-disable-next-line no-unused-vars
   const filePromise = await writeFile('note.txt', fileContents, 'utf8');
 }
