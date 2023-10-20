627229ad5e65bdfed713cfa042f76ed04365f34b
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-10-20 19:29:29 +0000

    WIP

---
+++ express-delete/server.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+
+type Grade = {
+  id: number;
+  name: string;
+  course: string;
+  score: number;
+};
+
+const grades: Record<number, Grade> = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95,
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100,
+  },
+  273: {
+    id: 273,
+    name: 'Håkon Wium Lie',
+    course: 'CSS',
+    score: 92,
+  },
+};
+
+const app = express();
+
+app.get((req, res, next) => {
+  const gradesArray: any = [];
+  for (const id in grades) {
+    gradesArray.push(grades[id]);
+  }
+  req.json(gradesArray);
+  next();
+});
+
+app.listen(8080, () => console.log('Listening on port 8080'));
