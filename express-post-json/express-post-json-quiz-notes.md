5b1e9bf934e4898db8e9fe69d3df10f7d4b4617d
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-10-22 20:07:32 +0000

    Completed express-post-json exercise -Kaitlin Lovrich

--- express-post-json/express-post-json-quiz-notes.md
+++ express-post-json/express-post-json-quiz-notes.md
@@ -5,31 +5,30 @@
 After completing this exercise, you should be able to discuss or answer the following questions:
 
 - What does the `express.json()` middleware do and when would you need it?
+  parses and srtingify's aka handles http requests
+  claas: looks for content type header, if it is application/json, if it is, it reads it, then parses, and that is the body
 
 ## Notes
 
-All student notes should be written here.
+app.post - create
+app.get - read
+app.put - update
+app.delete - delete
 
-How to write `Code Examples` in markdown
+req and res parameters are positional!!!!
 
-for JS:
+app.METHOD(PATH, HANDLER) ~basic routing, each route can have one or more handler functions, which are -executed- when the route is -matched-.
 
-```javascript
-const data = 'Howdy';
-```
+do `+variable` to convert a string variable into a number
 
-for HTML:
+res.status(404).json({error: `${id} not found`}) json sends status for you , no need to explicitly say sendStatus
 
-```html
-<div>
-  <p>This is text content</p>
-</div>
-```
+if you do app.use without a path, it will match for every path
 
-for CSS:
+do app.use(express.json()); ? to do what exactly?
 
-```css
-div {
-  width: 100%;
-}
-```
+# Notes/hints from the assignment directions:
+
+express.json() returns a “middleware”.
+app.use() is how you “mount” a middleware.
+Every middleware (or route) mounted after the JSON middleware will have an object on req.body.
--- express-post-json/server.ts
+++ express-post-json/server.ts
@@ -1,11 +1,94 @@
 import express from 'express';
 
-const app = express();
+type Grade = {
+  id: number;
+  name: string;
+  course: string;
+  score: number;
+};
 
-app.use((req, res, next) => {
-  console.log(req.method);
-  res.send('Hi Roberto!');
-  next();
+const app = express();
+app.use(express.json());
+
+let nextId = 1;
+const grades: Record<number, Grade> = {};
+
+app.get('/api/grades', (req, res) => {
+  res.json(grades);
 });
 
-app.listen(8080, () => console.log('Listening on port 8080'));
+app.post('/api/grades', (req, res) => {
+  const { name, course, score } = req.body;
+  if (!name || !course || score === undefined) {
+    res.status(400).json('Bad request');
+    return;
+  }
+  const grade = req.body;
+  grade.id = nextId;
+  grades[nextId++] = grade;
+  res.status(201).json(grade);
+});
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080!');
+});
+
+// Robert's solulu:
+
+// import express from 'express';
+// type Grade = {
+//   id: number;
+//   name: string;
+//   course: string;
+//   score: number;
+// };
+// const app = express();
+// app.use(express.json());
+// let nextId = 4;
+// const grades: Record<number, Grade> = {
+//   1: {
+//     id: 1,
+//     name: 'Tim Berners-Lee',
+//     course: 'HTML',
+//     score: 95,
+//   },
+//   3: {
+//     id: 3,
+//     name: 'Brendan Eich',
+//     course: 'JavaScript',
+//     score: 100,
+//   },
+// };
+// app.get('/api/grades', (req, res) => {
+//   res.json(Object.values(grades));
+// });
+// app.get('/api/gradesObj', (req, res) => {
+//   res.json(grades);
+// });
+// app.delete('/api/grades/:id', (req, res) => {
+//   const { id } = req.params;
+//   if (!Number.isInteger(id)) {
+//     res.status(400).json({ error: `${id} not an integer` });
+//     return;
+//   }
+//   if (!(id in grades)) {
+//     res.status(404).json({ error: `${id} not found` });
+//     return;
+//   }
+//   delete grades[+id];
+//   res.sendStatus(204);
+// });
+// app.post('/api/grades', (req, res) => {
+//   const { name, course, score } = req.body;
+//   if (!name || !course || score === undefined) {
+//     res.status(400).json('Bad request');
+//     return;
+//   }
+//   const grade = req.body;
+//   grade.id = nextId;
+//   grades[nextId++] = grade;
+//   res.status(201).json(grade);
+// });
+// app.listen(8080, () => {
+//   console.log('Listening on port 8080!');
+// });
