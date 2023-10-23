90751366f644d0cbde0a65663bf7fadc6b5e44b4
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-10-23 16:53:25 +0000

    Completed express-notes-api challenge -Kaitlin Lovrich

--- express-notes-api/server.ts
+++ express-notes-api/server.ts
@@ -8,8 +8,12 @@ app.use(express.json());
 const json = await readFile('data.json', 'utf8'); // string
 const data = JSON.parse(json); // obj
 
-console.log(data, writeFile); // to get rid of errors
+// Writes data to data.json
+async function writeData() {
+  await writeFile('data.json', JSON.stringify(data, null, 2), 'utf8');
+}
 
+// Reads all notes in the array
 app.get('/api/notes', (req, res) => {
   let note: any;
   const notesArray = [];
@@ -19,6 +23,7 @@ app.get('/api/notes', (req, res) => {
   res.status(200).json(notesArray);
 });
 
+// Reads the note specified by the inputted id
 app.get('/api/notes/:id', (req, res) => {
   const { id } = req.params;
   if (+id < 0) {
@@ -30,40 +35,66 @@ app.get('/api/notes/:id', (req, res) => {
   }
 });
 
+// Creates a new note and writes it to data.json
 app.post('/api/notes', async (req, res) => {
-  const { content } = req.body;
-  const note = req.body;
-  if (!content) {
-    res.status(400).json({ error: 'content is a required field' });
-  } else if (content) {
-    note.id = data.nextId;
-    data.notes[data.nextId++] = note;
-    res.status(201).json(data.notes[note.id]);
-    const dataNotesP = await writeFile(
-      'data.json',
-      JSON.stringify(data, null, 2),
-      'utf8'
-    );
+  try {
+    const { content } = req.body;
+    const note = req.body;
+    if (!content) {
+      res.status(400).json({ error: 'content is a required field' });
+    } else if (content) {
+      note.id = data.nextId;
+      data.notes[data.nextId++] = note;
+      res.status(201).json(data.notes[note.id]);
+      writeData();
+    }
+  } catch (err) {
+    console.error(Error);
+    res.status(500).json({ error: 'an unexpected error occurred.' });
   }
-  // if (note.id in data.notes) {
-  //   console.error(Error);
-  //   res.status(500).json({ error: 'an unexpected error occurred.' });
-  // }
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-  const { id } = req.params;
-  if (+id < 0) {
-    res.status(400).json({ error: `${id} not a positive integer` });
-  } else if (!(id in data.notes)) {
-    res.status(404).json({ error: `cannot find note with id: ${id}` });
-  } else {
-    delete data.notes[id];
-    res.status(204).json('note removed successfully');
+// Deletes the note specified by the inputted id and removes it from data.json
+app.delete('/api/notes/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (+id < 0) {
+      res.status(400).json({ error: `${id} not a positive integer` });
+    } else if (!(id in data.notes)) {
+      res.status(404).json({ error: `cannot find note with id: ${id}` });
+    } else {
+      delete data.notes[id];
+      res.sendStatus(204);
+      writeData();
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'an unexpected error occurred.' });
   }
-  // if (data.notes[note.id]) {
-  //   res.status(500).json({ error: 'an unexpected error occurred.' });
-  // }
+});
+
+// Updates the note specified by the inputted id
+app.put('/api/notes/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { content } = req.body;
+    const note = req.body;
+    if (+id < 0 || !content) {
+      res.status(400).json({
+        error: `${id} not a positive integer or content is null`,
+      });
+    } else if (!(id in data.notes)) {
+      res.status(404).json({ error: `cannot find note with id: ${id}` });
+    } else if (content) {
+      note.id = +id;
+      data.notes[id] = note;
+      res.status(200).json(data.notes[id]);
+      writeData();
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'an unexpected error occurred.' });
+  }
 });
 
 app.listen(8080, () => console.log('Listening on port 8080'));
