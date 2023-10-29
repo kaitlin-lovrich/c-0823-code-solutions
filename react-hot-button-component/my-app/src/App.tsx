7eacdf3008ac7e431cd5abe312aa098606733f1a
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-10-29 19:11:18 +0000

    Made changes Robert suggested and added Robert's solution at the bottom of HotButton.tsx for comparsion

--- react-hot-button-component/my-app/src/App.tsx
+++ react-hot-button-component/my-app/src/App.tsx
@@ -1,65 +1,8 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { useState } from 'react';
 import './App.css';
 import HotButton from './HotButton';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-
-  function changeColor(count: number): any {
-    let color;
-    if (count < 3) {
-      color = 'cold';
-    } else if (count < 6) {
-      color = 'cool';
-    } else if (count < 9) {
-      color = 'tepid';
-    } else if (count < 12) {
-      color = 'warm';
-    } else if (count < 15) {
-      color = 'hot';
-    } else if (count < 18) {
-      color = 'nuclear';
-    } else {
-      color = 'cold';
-      setCount((count = 0));
-    }
-    return color;
-  }
-
-  return (
-    <>
-      <HotButton
-        text="Hot Button"
-        customClick={handleClick}
-        color={changeColor(count)}
-      />
-    </>
-  );
+  return <HotButton text="Hot Button" />;
 }
 
 export default App;
-
-// /* eslint-disable @typescript-eslint/no-explicit-any */
-// import './App.css';
-// import HotButton from './HotButton';
-// import handleClick from './HotButton';
-// import changeColor from './HotButton';
-
-// function App() {
-//   return (
-//     <>
-//       <HotButton
-//         text="Hot Button"
-//         customClick={handleClick()}
-//         color={changeColor(count)}
-//       />
-//     </>
-//   );
-// }
-
-// export default App;
--- react-hot-button-component/my-app/src/HotButton.tsx
+++ react-hot-button-component/my-app/src/HotButton.tsx
@@ -1,23 +1,46 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
+import { useState } from 'react';
 import './HotButton.css';
 
 type ButtonProps = {
   text: string;
-  color: string;
-  customClick: () => void;
 };
 
-export default function HotButton({ text, color, customClick }: ButtonProps) {
+export default function HotButton({ text }: ButtonProps) {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  function changeColor(count: number) {
+    let color;
+    if (count < 3) {
+      color = 'cold';
+    } else if (count < 6) {
+      color = 'cool';
+    } else if (count < 9) {
+      color = 'tepid';
+    } else if (count < 12) {
+      color = 'warm';
+    } else if (count < 15) {
+      color = 'hot';
+    } else if (count < 18) {
+      color = 'nuclear';
+    } else {
+      color = 'cold';
+      setCount(0);
+    }
+    return color;
+  }
+
   return (
-    <button onClick={() => customClick()} className={color}>
+    <button onClick={() => handleClick()} className={changeColor(count)}>
       {text}
     </button>
   );
 }
 
-// colors[Math.floor(clicks/3) % VideoColorSpace.length];
-// above ^ whats left over (from the modulo operator) will never be bigger than the lentgth
+// Robert's Solulu:
 // import { useState } from 'react';
 // type Props = {
 //   label: string;
@@ -37,3 +60,5 @@ export default function HotButton({ text, color, customClick }: ButtonProps) {
 //     </>
 //   );
 // }
+// colors[Math.floor(clicks/3) % VideoColorSpace.length];
+// above ^ whats left over (from the modulo operator) will never be bigger than the lentgth
