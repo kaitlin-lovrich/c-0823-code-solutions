15697d8b16752167c01abcfdf00862d0c7a8c393
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-11-01 21:45:57 +0000

    Added Robert's solulu'

--- react-accordion-component/my-app/src/Accordion.tsx
+++ react-accordion-component/my-app/src/Accordion.tsx
@@ -25,7 +25,6 @@ function ShowTopics({ topics }: Props) {
       setActivePanel(topicId);
     }
   }
-
   const topicNames = topics.map((topic) => {
     return (
       <div>
@@ -43,3 +42,51 @@ function ShowTopics({ topics }: Props) {
 
   return <ul className="topics">{topicNames}</ul>;
 }
+
+// Robert's solulu:
+// import { useState } from 'react';
+// import './Accordion.css';
+// export type Topic = {
+//   id: number;
+//   title: string;
+//   content: string;
+// };
+// type Props = {
+//   topics: Topic[];
+// };
+// export function Accordion({ topics }: Props) {
+//   const [openTopic, setOpenTopic] = useState<Topic>();
+//   function handleClick(clickedTopic: Topic) {
+//     if (clickedTopic === openTopic) {
+//       setOpenTopic(undefined);
+//     } else {
+//       setOpenTopic(clickedTopic);
+//     }
+//   }
+//   return (
+//     <div>
+//       {topics.map((topic) => (
+//         <TopicCard
+//           topic={topic}
+//           isOpen={openTopic === topic}
+//           onClick={handleClick}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
+// type CardProps = {
+//   topic: Topic;
+//   isOpen: boolean;
+//   onClick: (topic: Topic) => void;
+// };
+// function TopicCard({ topic, isOpen, onClick }: CardProps) {
+//   return (
+//     <div className="topic">
+//       <div onClick={() => onClick(topic)} className="topic-title">
+//         <h3>{topic.title}</h3>
+//       </div>
+//       {isOpen && <div className="topic-content">{topic.content}</div>}
+//     </div>
+//   );
+// }
