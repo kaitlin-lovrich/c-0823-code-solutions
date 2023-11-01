deae826333a2f95574d27b7ac51c6da53c9c21a9
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-11-01 17:09:20 +0000

    Added notes during class discussion

--- react-stopwatch-component/my-app/src/Stopwatch.tsx
+++ react-stopwatch-component/my-app/src/Stopwatch.tsx
@@ -64,9 +64,60 @@ function StartStop({ running, onClick }: StartStopProps) {
   );
 }
 
+// My notes before getting started:
 // need state for TIME and START/STOP (isRunning) V
 // setTimeout for 1 second or interval
 // set state of your counter +1 V
 // click pause,
 // need a state to clear an interval id
 // if they click start, save the interval id
+
+// Robert's Solulu:
+// import { useState } from 'react';
+// import { FaPause, FaPlay } from 'react-icons/fa';
+// import './StopWatch.css';
+// export function StopWatch() {
+//   const [elapsedTime, setElapsedTime] = useState(7);
+//   const [intervalId, setIntervalId] = useState<NodeJS.Timer>();
+//   function handlePlayClick() {
+//     if (intervalId === undefined) {
+//       const id = setInterval(() => setElapsedTime((prev) => prev + 1), 1000);
+//       setIntervalId(id);
+//     } else {
+//       clearInterval(intervalId);
+//       setIntervalId(undefined);
+//     }
+//   }
+//   const cursor = intervalId === undefined ? 'pointer' : 'default';
+//   return (
+//     <div className="stopwatch">
+//       <div
+//         className="watch-face"
+//         style={{ cursor }}
+//         onClick={() => intervalId === undefined && setElapsedTime(0)}>
+//         <ElapsedTime time={elapsedTime} />
+//       </div>
+//       <PlayButton
+//         isRunning={intervalId !== undefined}
+//         onClick={handlePlayClick}
+//       />
+//     </div>
+//   );
+// }
+// type TimeProps = {
+//   time: number;
+// };
+// function ElapsedTime({ time }: TimeProps) {
+//   return <div>{time}</div>;
+// }
+// type ButtonProps = {
+//   isRunning: boolean;
+//   onClick: () => void;
+// };
+// function PlayButton({ isRunning, onClick }: ButtonProps) {
+//   return (
+//     <div onClick={onClick} className="start-stop">
+//       {isRunning ? <FaPause /> : <FaPlay />}
+//     </div>
+//   );
+// }
