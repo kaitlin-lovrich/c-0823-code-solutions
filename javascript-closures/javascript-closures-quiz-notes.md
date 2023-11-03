84c5f68b6ce936120efd892eb2355706813ac785
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-11-03 02:06:33 +0000

    Completed javascript-closures exercise -Kaitlin Lovrich

--- javascript-closures/javascript-closures-quiz-notes.md
+++ javascript-closures/javascript-closures-quiz-notes.md
@@ -5,15 +5,16 @@
 After completing this exercise, you should be able to discuss or answer the following questions:
 
 - In JavaScript, when is scope determined?
-
+  scoope is determined in javascript at lexical time
 - What allows JavaScript functions to "remember" variables from their surroundings?
-
+  closures
 - What values does a closure contain?
-
+  the variables and non-javascript global functions (like setTimeout) that are outside of a function, that the function utilizes, goes in the closure
 - When is a closure created?
-
+  the closure gets created when the function is defined
 - How can you tell if a function will be created with a closure?
-
+  it will be created if it accesses anythin g outside of itself that is not a global scope
 - In React, what is one important case where you need to know if a closure was created?
+  when you are writing out your dependency array for your useEffect
 
 ## Notes
--- javascript-closures/my-app/src/Counter.tsx
+++ javascript-closures/my-app/src/Counter.tsx
@@ -1,17 +1,38 @@
 /* eslint-disable react-hooks/exhaustive-deps -- Delete this line! */
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export function Counter() {
   const [counter, setCounter] = useState(0);
+  // NEW HOOK!
+  const getData = useCallback(() => {
+    return { foo: 'bar' };
+  }, []);
   useEffect(() => {
     getData();
     setCounter((prev) => prev + 1);
   }, [getData]);
 
-  function getData() {
-    // fetch data
-    return { foo: 'bar' };
-  }
+  // function getData() {
+  //   // fetch data
+  //   return { foo: 'bar' };
+  // }
 
   return <div>Fetching {counter} times</div>;
 }
+
+// Below shows an exmple of using a dependency for the `useCallback` HOOK:
+// import { useCallback, useEffect, useState } from 'react';
+
+// export function Counter() {
+//   const [counter, setCounter] = useState(0);
+//   // NEW HOOK!
+//   const getData = useCallback(() => {
+//     console.log(counter);
+//     return { foo: 'bar' };
+//   }, [counter]); // You only need counter variable inside of dependencies because it is referencing outside of the useCallback callback function
+//   useEffect(() => {
+//     getData();
+//     setCounter((prev) => prev + 1);
+//   }, [getData]);
+//   return <div>Fetching {counter} times</div>;
+// }
