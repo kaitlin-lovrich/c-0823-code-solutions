8ac5824e2065d149753faddf870dc56d9e6d730a
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-11-06 18:00:11 +0000

    Completed fetch exercise -Kaitlin Lovrich'

--- fetch/fetch-quiz-notes.md
+++ fetch/fetch-quiz-notes.md
@@ -11,5 +11,8 @@ After completing this exercise, you should be able to discuss or answer the foll
 - How do you specify the request method (`GET`, `POST`, etc.) when calling `fetch`?
   specify the method as the second parameter of the fetch method. example: fetch(resource, "POST")
 - How does `fetch` report errors?
+  A fetch() promise will reject with a TypeError when a network error is encountered or CORS is misconfigured on the server-side.
+  fetch will not reject if the server returns a 404 or 500. Instead, it resolves and sets the ok property to false.
+  access by using response.ok (it returns either true or false)
 
 ## Notes
--- fetch/main.js
+++ fetch/main.js
@@ -1,7 +1,32 @@
 async function logUserObjs() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const userObjects = await response.json();
-  console.log(userObjects);
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+    if (response.ok) {
+      const userObjects = await response.json();
+      console.log('userObjects:', userObjects);
+    } else {
+      throw new Error(response.status);
+    }
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 logUserObjs();
+
+async function logPokemon() {
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
+    if (response.ok) {
+      const pokemon = await response.json();
+      console.log('pokemon:', pokemon);
+    } else {
+      throw new Error(response.status);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+logPokemon();
