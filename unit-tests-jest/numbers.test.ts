5d48d36d59cea3de536e7ff83d9eead1427c2cae
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-11-14 05:04:43 +0000

    WIP

--- unit-tests-jest/numbers.test.ts
+++ unit-tests-jest/numbers.test.ts
@@ -1,18 +1,23 @@
 import { evenNumbers, toDollars, divideBy, multiplyBy } from './numbers';
 
-test('Expect [1, 2, 3, 4, 5, 6]', () => {
-  expect(() => evenNumbers([1, 2, 3, 4, 5, 6])).toBe([2, 4, 6]);
+describe('evenNumbers', () => {
+  it('Expect [1, 2, 3, 4, 5, 6]', () => {
+    expect(() => evenNumbers([1, 2, 3, 4, 5, 6])).toBe([2, 4, 6]);
+  });
 });
 
-test('Expect 4', () => {
+describe('toDollars', () => {
+  it('Expect 4');
   expect(() => toDollars(4)).toBe('$4.00');
 });
 
-test('Expect [10, 12, 14, 16], 2', () => {
+describe('divideBy', () => {
+  it('Expect [10, 12, 14, 16], 2');
   expect(() => divideBy([10, 12, 14, 16], 2)).toBe([5, 6, 7, 8]);
 });
 
-test('Expect {first: 3, second: "four"}, 2', () => {
+describe('multiplyBy', () => {
+  it('Expect {first: 3, second: "four"}, 2');
   expect(() => multiplyBy({ first: 3, second: 'four' }, 2)).toBe({
     first: 6,
     second: 'four',
